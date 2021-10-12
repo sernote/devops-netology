38056cf07f8496b3850561ae497795b485a792c3
notevskii <notevskii@bitrix.ru>
2021-10-12 23:37:44 +0200

    terraform: добавил модуль ec2_instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,22 @@ resource "aws_instance" "web_for_each" {
   ami = each.value
 }
 
+resource "aws_instance" "web_for_each" {
+  for_each = local.instances
+
+  instance_type = each.key
+  ami = each.value
+}
+
 data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "web_by_module"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+}
